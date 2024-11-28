25afa31f643184f8cde6bc3f8857f6395c97410e
davidmarsoni <marsoni.david@gmail.com>
2024-11-28 18:28:12 +0100

    Move project in the solution to have a better structure

---
+++ 2024TDD04/2024TDD04.DAL.Tests/Models/RoomAccessContextTest.cs
@@ -0,0 +1,13 @@
+using DAL;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace _2024TDD04.DAL.Tests.Models
+{
+    public class RoomAccessContextTest
+    {
+
+    }
+}
---
+++ 2024TDD04/2024TDD04.DAL.Tests/Models/UserGroupTest.cs
@@ -0,0 +1,77 @@
+﻿using DAL.Models;
+using Group = DAL.Models.Group;
+
+namespace _2024TDD04.DAL.Tests.Models
+{
+    public class UserGroupTests
+    {
+        [Fact]
+        public void UserGroup_DefaultValues_ShouldBeInitialized()
+        {
+            // Arrange
+            var userGroup = new UserGroup();
+
+            // Act & Assert
+            Assert.Equal(0, userGroup.UserId);
+            Assert.Null(userGroup.User);
+            Assert.Equal(0, userGroup.GroupId);
+            Assert.Null(userGroup.Group);
+        }
+
+        [Fact]
+        public void UserGroup_SetUserId_ShouldReturnCorrectValue()
+        {
+            // Arrange
+            var userGroup = new UserGroup();
+            var expectedUserId = 1;
+
+            // Act
+            userGroup.UserId = expectedUserId;
+
+            // Assert
+            Assert.Equal(expectedUserId, userGroup.UserId);
+        }
+
+        [Fact]
+        public void UserGroup_SetUser_ShouldReturnCorrectValue()
+        {
+            // Arrange
+            var userGroup = new UserGroup();
+            var expectedUser = new User { Id = 1, Username = "Test User" };
+
+            // Act
+            userGroup.User = expectedUser;
+
+            // Assert
+            Assert.Equal(expectedUser, userGroup.User);
+        }
+
+        [Fact]
+        public void UserGroup_SetGroupId_ShouldReturnCorrectValue()
+        {
+            // Arrange
+            var userGroup = new UserGroup();
+            var expectedGroupId = 1;
+
+            // Act
+            userGroup.GroupId = expectedGroupId;
+
+            // Assert
+            Assert.Equal(expectedGroupId, userGroup.GroupId);
+        }
+
+        [Fact]
+        public void UserGroup_SetGroup_ShouldReturnCorrectValue()
+        {
+            // Arrange
+            var userGroup = new UserGroup();
+            var expectedGroup = new Group { Id = 1, Name = "Test Group", Acronym = "TG" };
+
+            // Act
+            userGroup.Group = expectedGroup;
+
+            // Assert
+            Assert.Equal(expectedGroup, userGroup.Group);
+        }
+    }
+}
